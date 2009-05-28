d84bf2e488b76a21b1db188ee3d508b355aa6d9f
Benson Kalahar <benson@WE12552.pnl.gov>
2009-05-27 17:42:06 -0700

    Made JMS work properly.

--- src/main/java/org/systemsbiology/gaggle/GaggleDriver.java
+++ src/main/java/org/systemsbiology/gaggle/GaggleDriver.java
@@ -1,28 +1,22 @@
 package org.systemsbiology.gaggle;
 import gov.pnnl.mif.api.exception.MifException;
 import gov.pnnl.mif.api.MifPipeline;
+import gov.pnnl.mif.api.MifJmsConnector.JmsProvider;
 
 public class GaggleDriver {
   public static void main(String[] args) throws MifException {
     MifPipeline pipeline = new MifPipeline();
+		pipeline.addMifJmsConnector("tcp://localhost:61616", JmsProvider.ACTIVEMQ);
 
-		pipeline.addMifModule(org.systemsbiology.gaggle.admin.JmsIngest.class.getName(),
-				"jms://topic/admin", "vm://adminHandler");
-		pipeline.addMifModule(org.systemsbiology.gaggle.admin.Handler.class.getName(),
-				"vm://adminHandler", "vm://adminJmsOut");
-		pipeline.addMifModule(org.systemsbiology.gaggle.admin.JmsEmitter.class.getName(),
-				"vm://adminJmsOut", "jms://topic/adminOut");
+		pipeline.addMifModule(org.systemsbiology.gaggle.admin.JmsIngest.class.getName(), "jms://topic:adminSend", "vm://adminHandler");
+		pipeline.addMifModule(org.systemsbiology.gaggle.admin.Handler.class.getName(), "vm://adminHandler", "vm://adminJmsOut");
+		pipeline.addMifModule(org.systemsbiology.gaggle.admin.JmsEmitter.class.getName(), "vm://adminJmsOut", "jms://topic:admin");
 
-		pipeline.addMifModule(org.systemsbiology.gaggle.boss.JmsIngester.class.getName(),
-				"vm://ingest", "vm://annotate");
-		pipeline.addMifModule(org.systemsbiology.gaggle.boss.Handler.class.getName(),
-				"vm://ingest", "vm://annotate");
-		pipeline.addMifModule(org.systemsbiology.gaggle.boss.Annotator.class.getName(),
-				"vm://annotate", "vm://postProcess");
-		pipeline.addMifModule(org.systemsbiology.gaggle.boss.PostProcessor.class.getName(),
-				"vm://postProcess", "vm://emitJMS");
-		pipeline.addMifModule(org.systemsbiology.gaggle.boss.JmsEmitter.class.getName(),
-				"vm://emitJMS", "jms://topic/items");
+		pipeline.addMifModule(org.systemsbiology.gaggle.boss.JmsIngester.class.getName(), "jms://topic:send", "vm://ingest");
+		pipeline.addMifModule(org.systemsbiology.gaggle.boss.Handler.class.getName(), "vm://ingest", "vm://annotate");
+		pipeline.addMifModule(org.systemsbiology.gaggle.boss.Annotator.class.getName(), "vm://annotate", "vm://postProcess");
+		pipeline.addMifModule(org.systemsbiology.gaggle.boss.PostProcessor.class.getName(), "vm://postProcess", "vm://emitJMS");
+		pipeline.addMifModule(org.systemsbiology.gaggle.boss.JmsEmitter.class.getName(), "vm://emitJMS", "jms://topic:items");
 
     pipeline.start();
   }
