486169938eb80aa02407deb7957aa823d52a17cf
Benson Kalahar <benson@WE12552.pnl.gov>
2009-06-02 16:52:57 -0700

    Fixed some tab/spaces inconsistencies.

--- src/main/java/org/systemsbiology/gaggle/admin/Handler.java
+++ src/main/java/org/systemsbiology/gaggle/admin/Handler.java
@@ -5,14 +5,14 @@ import java.util.HashMap;
 import gov.pnnl.mif.api.types.MifProcessor;
 
 public class Handler implements MifProcessor {
-	public Serializable listen(Serializable input) {
-		return input;
-	}
+  public Serializable listen(Serializable input) {
+    return input;
+  }
 
-	public String getInputType() {
-		return Serializable.class.getName();
-	}
-	public String getOutputType() {
-		return Serializable.class.getName();
-	}
+  public String getInputType() {
+    return Serializable.class.getName();
+  }
+  public String getOutputType() {
+    return Serializable.class.getName();
+  }
 }
